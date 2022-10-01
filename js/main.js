a04d21f7668a15cf9570f2b54a92d991aa40036f
restby <restby@gmail.com>
2022-10-01 21:32:02 +0300

    добавляет функции получения случайного числа из интервала

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0) {
+    return NaN
+  }
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const randomInt = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(randomInt);
+}
+
+console.log(getRandomInt(1, 5));
+
+const getRandomFloat = (min, max, count = 1) => {
+  if (min < 0 || max < 0 || count < 0) {
+    return NaN
+  }
+  const lower = Math.min(min, max);
+  const upper = Math.max(min, max);
+  const randomFlaot = Math.random() * (upper - lower) + lower;
+  return Number(randomFlaot.toFixed(count));
+}
+
+console.log(getRandomFloat(1, 3, 2));
