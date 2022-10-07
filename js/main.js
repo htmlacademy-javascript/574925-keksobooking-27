2730f690533ed2f79acf2f08fdd4307fb8c694fa
restby <restby@gmail.com>
2022-10-07 14:48:05 +0300

    task 4.15

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ const getRandomInt = (min, max) => {
   const randomInt = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(randomInt);
 };
-
 getRandomInt(1, 5);
 
 const getRandomFloat = (min, max, count = 1) => {
@@ -25,5 +24,32 @@ const getRandomFloat = (min, max, count = 1) => {
   const randomFloat = Math.random() * (upper - lower) + lower;
   return Number(randomFloat.toFixed(count));
 };
-
 getRandomFloat(1, 3, 2);
+
+// const ANNOUNCEMENT_COUNT = 10;
+/**
+ *  *
+ * const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+ * const createOffer = () => ({
+    author: '',
+    offer:  '',
+    location:  '',
+  });
+ * const announcements = Array.from({length: ANNOUNCEMENT_COUNT}, createOffer);
+ */
+
+const randomIntUrl = (String(getRandomInt(1, 10)).padStart(2, '0'));
+const createAuthor = () => ({
+  avatar: `img/avatars/user${randomIntUrl}.png`
+});
+const offerTitle = 'Обьявление аренды';
+
+
+const createOffer = () => ({
+  author: createAuthor(),
+  title: offerTitle,
+  price: getRandomInt(100, 1000),
+  rooms: getRandomInt(1, 5),
+});
+createOffer();
+// console.log(createOffer());
