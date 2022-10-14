25cd0e8a33f2a17000cfd346183a43604af4b995
restby <restby@gmail.com>
2022-10-14 23:44:12 +0300

    прошлые правки

--- js/data.js
+++ js/data.js
@@ -7,9 +7,9 @@ import {
 
 const ANNOUNCEMENT_COUNT = 10;
 
-const offerTitle = ['Объявление аренды квартиры', 'Объявление аренды дворца', 'Объявление аренды комнаты', 'Объявление аренды дома', 'Объявление аренды бунгало', 'Объявление аренды коттеджа', 'Объявление аренды палатки', 'Объявление аренды гостиничного номера', 'Объявление аренды картонной коробки', 'Объявление аренды собачьей будки'];
+const OFFER_TITLE = ['Объявление аренды квартиры', 'Объявление аренды дворца', 'Объявление аренды комнаты', 'Объявление аренды дома', 'Объявление аренды бунгало', 'Объявление аренды коттеджа', 'Объявление аренды палатки', 'Объявление аренды гостиничного номера', 'Объявление аренды картонной коробки', 'Объявление аренды собачьей будки'];
 
-const offerDescription = ['Красивая квартира для встреч на сутки', 'Норма хата загудеть на выходные', 'Дешманский номер в захудалой гостиннице', 'Брезентовая палатка для отдыха на природе', 'Переносная коробка для ночевок на улице', 'Домик с комарами в лесу', 'Крутой дворец мажориков', 'Будка вместе с собачьей охраной', 'Крыша из сена, но зато у моря', 'Специально для вас, наша конура по цене дворца',];
+const OFFER_DESCRIPTION = ['Красивая квартира для встреч на сутки', 'Норма хата загудеть на выходные', 'Дешманский номер в захудалой гостиннице', 'Брезентовая палатка для отдыха на природе', 'Переносная коробка для ночевок на улице', 'Домик с комарами в лесу', 'Крутой дворец мажориков', 'Будка вместе с собачьей охраной', 'Крыша из сена, но зато у моря', 'Специально для вас, наша конура по цене дворца',];
 
 const Price = {
   MIN: 1,
@@ -28,12 +28,12 @@ const Location = {
   MAX_LONGITUDE: LNG_MAX,
 };
 
-const typeOfBuilding = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const checkinTime = ['12:00', '13:00', '14:00'];
-const checkoutTime = ['12:00', '13:00', '14:00'];
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const TYPE_OF_BUILDING = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN_TIME = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 const randomIntUrl = (String(getRandomInt(1, 10)).padStart(2, '0'));
 
@@ -47,17 +47,17 @@ const createLocationData = () => ({
 });
 
 const createOfferData = () => ({
-  title: getRandomValue(offerTitle),
+  title: getRandomValue(OFFER_TITLE),
   address: `${getRandomFloat(Location.MIN_LATITUDE, Location.MAX_LATITUDE, 5)}, ${getRandomFloat(Location.MIN_LONGITUDE, Location.MAX_LONGITUDE, 5)}`,
   price: getRandomInt(Price.MIN, Price.MAX),
   rooms: getRandomInt(1, 3),
-  type: getRandomValue(typeOfBuilding),
+  type: getRandomValue(TYPE_OF_BUILDING),
   guests: getRandomInt(1, 3),
-  checkin: getRandomValue(checkinTime),
-  checkoutTime: getRandomValue(checkoutTime),
-  description: getRandomValue(offerDescription),
-  features: createRandomUniqArray(features),
-  photos: createRandomUniqArray(photos)
+  checkin: getRandomValue(CHECKIN_TIME),
+  checkoutTime: getRandomValue(CHECKOUT_TIME),
+  description: getRandomValue(OFFER_DESCRIPTION),
+  features: createRandomUniqArray(FEATURES),
+  photos: createRandomUniqArray(PHOTOS)
 });
 
 const getOffer = () => ({
