d0864ed5e8eaff589d50b32fbbbf9da6c1bad1db
restby <restby@gmail.com>
2022-10-31 18:22:13 +0300

    some com

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ import { renderCardOffer } from './template.js';
 import { disabledAdForm } from './form.js';
 import { disabledMapFilter } from './filter.js';
 renderCardOffer();
-disabledAdForm();
-disabledMapFilter();
+// disabledAdForm();
+// disabledMapFilter();
