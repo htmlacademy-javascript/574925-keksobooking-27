65298f4bc649cddb7a370f6bd1d972057340baf9
restby <restby@gmail.com>
2022-10-31 14:58:36 +0300

    pristine

--- js/main.js
+++ js/main.js
@@ -3,5 +3,26 @@ import { renderCardOffer } from './template.js';
 import { disabledAdForm } from './form.js';
 import { disabledMapFilter } from './filter.js';
 renderCardOffer();
-disabledAdForm();
+// disabledAdForm();
 disabledMapFilter();
+
+const adFormElement = document.querySelector('.ad-form');
+const pristine = new Pristine(adFormElement, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'text-help'
+});
+
+adFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('Можно отправлять');
+  } else {
+    console.log('Форма невалидна');
+  }
+});
