09be70d6ae1e5088a8b266906ed071cfcd303d81
restby <restby@gmail.com>
2022-11-01 07:42:30 +0300

    3

--- js/main.js
+++ js/main.js
@@ -21,6 +21,29 @@ const validateTitle = (value) => value.length >= 30 && value.length <= 100;
 pristine.addValidator(adFormElement.querySelector('#title'), validateTitle,
   'Длинна заголовка должна быть от 30 до 100 символов');
 
+/**
+ * const roomsField = orderForm.querySelector('[name="rooms"]');
+ * const capacityField = orderForm.querySelector('[name="capacity"]');
+const capacityOption = {
+  '1 комната': ['для 1 гостя'],
+  '2 комнаты': ['для 2 гостей', 'для 1 гостя'],
+  '3 комнаты': ['для 3 гостей' ,'для 2 гостей', 'для 1 гостя'],
+  '100 комнат': ['не для гостей'],
+};
+function validateRooms () {
+  return capacityOption[capacityField.value].includes(capacityField.value);
+function getRoomsErrorMessage () {
+  return `
+    ${roomsField.value}
+    ${dateField.value.toLowerCase()}
+    ${roomsField.value === 'Доставка' ? 'невозможна' : 'невозможен'}
+  `;
+}
+
+pristine.addValidator(roomsField, validateRooms);
+pristine.addValidator(capacityField, validateRooms);
+ */
+
 
 adFormElement.addEventListener('submit', (evt) => {
   evt.preventDefault();
