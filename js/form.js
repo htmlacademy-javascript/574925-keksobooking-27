6348e8c33bf313caab04b3d8d5960933b72e7d0d
restby <restby@gmail.com>
2022-11-05 18:57:33 +0300

    m9t1

--- js/form.js
+++ js/form.js
@@ -1,39 +1,8 @@
-/* eslint-disable no-console */
 const adFormElement = document.querySelector('.ad-form');
 const adFormInputElements = adFormElement.querySelectorAll('fieldset');
-
-const disabledAdForm = () => {
-  adFormElement.classList.add('ad-form--disabled');
-  adFormInputElements.forEach((adFormInput) => {
-    adFormInput.disabled = true;
-  });
-};
-
-const enabledAdForm = () => {
-  adFormElement.classList.remove('ad-form--disabled');
-  adFormInputElements.forEach((adFormInput) => {
-    adFormInput.disabled = false;
-  });
-};
-
 const addressElement = document.querySelector('#address');
-
 const roomsElement = document.querySelector('#room_number');
-
 const capacityElement = document.querySelector('#capacity');
-
-const onAddressFocus = () => {
-  addressElement.blur();
-};
-
-const pristine = new Pristine(adFormElement, {
-  classTo: 'ad-form__element',
-  errorClass: 'ad-form__element--invalid',
-  successClass: 'ad-form__element--valid',
-  errorTextParent: 'ad-form__element',
-  errorTextTag: 'span',
-  errorTextClass: 'text-help'
-}, true);
 
 const roomsForGuests = {
   1: ['1'],
@@ -49,10 +18,36 @@ const guetsForRooms = {
   3: ['3'],
 };
 
+const disabledAdForm = () => {
+  adFormElement.classList.add('ad-form--disabled');
+  adFormInputElements.forEach((adFormInput) => {
+    adFormInput.disabled = true;
+  });
+};
+
+const enabledAdForm = () => {
+  adFormElement.classList.remove('ad-form--disabled');
+  adFormInputElements.forEach((adFormInput) => {
+    adFormInput.disabled = false;
+  });
+};
+
+const onAddressFocus = () => {
+  addressElement.blur();
+};
+
+const pristine = new Pristine(adFormElement, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'text-help'
+}, true);
+
 const validateCapacity = () => roomsForGuests[roomsElement.value].includes(capacityElement.value);
 
 const getCapacityErrorMessage = () => `Указанное количество комнат вмещает ${roomsForGuests[roomsElement.value].join(' или ')} ${Number(roomsForGuests[roomsElement.value]) === 0 ? 'гостей' : 'гостя'}.`;
-
 
 const getRoomsErrorMessage = () => `Для указанного количества гостей требуется ${guetsForRooms[capacityElement.value].join(' или ')} ${Number(guetsForRooms[capacityElement.value]) === 100 ? 'комнат' : 'комнаты'}.`;
 
@@ -60,6 +55,7 @@ const onCapacityChange = () => {
   pristine.validate(capacityElement);
   pristine.validate(roomsElement);
 };
+
 const onRoomsChange = () => {
   pristine.validate(capacityElement);
   pristine.validate(roomsElement);
@@ -73,6 +69,10 @@ addressElement.addEventListener('focus', onAddressFocus);
 roomsElement.addEventListener('change', onRoomsChange);
 capacityElement.addEventListener('change', onCapacityChange);
 
+const setAddress = ({ lat, lng }) => {
+  addressElement.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
+};
+
 adFormElement.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
@@ -84,4 +84,4 @@ adFormElement.addEventListener('submit', (evt) => {
   }
 });
 
-export { disabledAdForm, enabledAdForm };
+export { disabledAdForm, enabledAdForm, setAddress };
--- js/main.js
+++ js/main.js
@@ -1,72 +1,24 @@
 /* eslint-disable no-console */
-import { renderCardOffer } from './template.js';
-import { disabledAdForm, enabledAdForm } from './form.js';
+import { getOffers } from './data.js';
+import { disabledAdForm, enabledAdForm, setAddress } from './form.js';
 import { disabledMapFilter, enabledMapFilter } from './filter.js';
-renderCardOffer();
+import { initMap, setOnMapLoad, setOnMainPinMove, setOtherPins } from './map.js';
+
+const START_COORDINATE = {
+  lat: 35.70000,
+  lng: 139.42500,
+};
+
+const offers = getOffers();
+
+setOnMapLoad(() => {
+  enabledAdForm();
+  enabledMapFilter();
+  setOtherPins(offers);
+  setOnMainPinMove(setAddress);
+  setAddress(START_COORDINATE);
+});
+
 disabledAdForm();
 disabledMapFilter();
-
-const resetButtonElement = document.querySelector('.ad-form__reset');
-const addressElement = document.querySelector('#address');
-
-const mapElement = L.map('map-canvas')
-  .on('load', () => {
-    enabledAdForm();
-    enabledMapFilter();
-    console.log('load map');
-
-  })
-  .setView({
-    lat: 35.70000,
-    lng: 139.42500,
-  }, 10);
-
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(mapElement);
-
-const mainPinIcon = L.icon({
-  iconUrl: './img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
-
-// const otherPinIcon = L.icon({
-//   iconUrl: './img/pin.svg',
-//   iconSize: [40, 40],
-//   iconAnchor: [20, 40],
-// });
-
-
-const mainPinMarker = L.marker(
-  {
-    lat: 35.70000,
-    lng: 139.42500,
-  },
-  {
-    draggable: true,
-    icon: mainPinIcon,
-  },
-);
-
-mainPinMarker.addTo(mapElement);
-
-mainPinMarker.on('moveend', (evt) => {
-  addressElement.value = evt.target.getLatLng();
-
-});
-
-resetButtonElement.addEventListener('click', () => {
-  mainPinMarker.setLatLng({
-    lat: 35.70000,
-    lng: 139.42500,
-  });
-
-  mapElement.setView({
-    lat: 35.70000,
-    lng: 139.42500,
-  }, 16);
-});
+initMap(START_COORDINATE);
