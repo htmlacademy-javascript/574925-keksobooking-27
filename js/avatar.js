5ddd7f30af30fb81667fc63699584654f52d8d21
restby <restby@gmail.com>
2022-11-14 22:29:51 +0300

    lint

--- js/avatar.js
+++ js/avatar.js
@@ -1,8 +1,6 @@
 import { setImages } from './utils.js';
 
 const setAvatar = () => {
-  const FILE_TYPES = ['jpg', 'jpeg', 'png'];
-
   const fileChooserElement = document.querySelector('#avatar');
   const previewElement = document.querySelector('.ad-form-header__preview img');
 
